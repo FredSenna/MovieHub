b9d6cf65ebe203d1571e7dce3614a167c7e8e7d6
FredSenna <luccacansen@hotmail.com>
2024-02-04 20:45:00 -0300

    Adicionando projeto

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import './header.css';
+import { Link } from 'react-router-dom'
+
+function Header(){
+  return(
+    <header elementClass="header">
+      <Link className="logo" to="/">
+        <img src='./Logo moviehub site.png'/>
+      </Link>
+      <Link className="favoritos" to="/favoritos">Meus filmes</Link>
+    </header>
+  )
+}
+
+export default Header;
